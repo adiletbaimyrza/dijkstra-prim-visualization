6afe064f5e2a78c5305196d95cb1d534024a0538
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-11-06 18:06:10 +0100

    Add Navbar component to App.jsx

--- dijkstra-prim-visualization/src/App.jsx
+++ dijkstra-prim-visualization/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Canvas from "./components/Canvas/Canvas";
+import Navbar from "./components/Navbar/Navbar";
 import { GraphParamsProvider } from "./GraphParamsContext";
 
 function App() {
   return (
     <GraphParamsProvider>
+      <Navbar />
       <Canvas />
     </GraphParamsProvider>
   );
