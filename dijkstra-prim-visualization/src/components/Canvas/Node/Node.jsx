47dcb5451723f61f0df9a0b6b875aeee4485e5ad
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-11-09 20:48:57 +0100

    Refactor Node component props to include id.

--- dijkstra-prim-visualization/src/components/Canvas/Node/Node.jsx
+++ dijkstra-prim-visualization/src/components/Canvas/Node/Node.jsx
@@ -2,19 +2,23 @@ import PropTypes from "prop-types";
 import classes from "./Node.module.css";
 
 /**
- * A component that renders a circle representing a node on the canvas.
- * @param {number} cx - The x-coordinate of the center of the circle.
- * @param {number} cy - The y-coordinate of the center of the circle.
- * @param {function} onNodeClick - A callback function to handle click events on the node.
+ * Renders a node on the canvas.
+ * @param {Object} props - The props object.
+ * @param {string} props.id - The id of the node.
+ * @param {number} props.cx - The x-coordinate of the node.
+ * @param {number} props.cy - The y-coordinate of the node.
+ * @param {function} props.onNodeClick - The function to call when the node is clicked.
+ * @returns {JSX.Element} - The node element.
  */
-const Node = ({ cx, cy, onNodeClick }) => {
+const Node = ({ id, cx, cy, onNodeClick }) => {
   return (
     <circle
       className={classes.circle}
+      id={id}
       cx={cx}
       cy={cy}
       r="14" // TODO: Make the radius a prop to allow for customization
-      onClick={(event) => onNodeClick(event, { x: cx, y: cy })}
+      onClick={(event) => onNodeClick(event, { id: id, x: cx, y: cy })}
     />
   );
 };
@@ -24,6 +28,7 @@ export default Node;
 // Ensures that the required props are passed and have the correct type
 Node.propTypes = {
   onNodeClick: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
   cx: PropTypes.number.isRequired,
   cy: PropTypes.number.isRequired,
 };
