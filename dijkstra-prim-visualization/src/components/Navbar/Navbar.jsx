aa3f07d314b30704a811c80b71c431ba10853984
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-11-13 18:55:58 +0100

    framework for testing

--- dijkstra-prim-visualization/src/components/Navbar/Navbar.jsx
+++ dijkstra-prim-visualization/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import { useContext } from "react";
 import { GraphParamsContext } from "../../GraphParamsContext";
+import { createGraph } from "../../algorithms/graph";
+import prim from "../../algorithms/mstPrim";
+import dijkstra from "../../algorithms/spDijkstra";
 
 /**
  * Navbar component displays buttons to print nodes and edges.
@@ -7,8 +10,19 @@ import { GraphParamsContext } from "../../GraphParamsContext";
  */
 const Navbar = () => {
   const { nodes, edges } = useContext(GraphParamsContext);
+
+  const runPrim = () => {
+    // you can run your code there
+  };
+
+  const runDijkstra = () => {
+    // you can run your code there
+  };
+
   return (
     <>
+      <button onClick={runPrim}>Run prim's algorithm</button>
+      <button onClick={runDijkstra}>Run dijkstra's algorithm</button>
       <button
         onClick={() => {
           console.log(nodes);
