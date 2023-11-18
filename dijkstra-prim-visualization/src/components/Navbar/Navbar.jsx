ee0111da45a64dcc785ac60d1d178e8b5740c4f1
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-11-18 17:20:28 +0100

    Refactor Navbar component to use animations

--- dijkstra-prim-visualization/src/components/Navbar/Navbar.jsx
+++ dijkstra-prim-visualization/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,29 @@
 import { useContext } from "react";
 import { GraphParamsContext } from "../../GraphParamsContext";
-import { createGraphFromComponent } from "../../algorithms/graph";
-import prim from "../../algorithms/mstPrim";
-import dijkstra from "../../algorithms/spDijkstra";
+import { startAnimations } from "./animations";
+import { runDijkstra, runPrim } from "./NavbarUtils";
 
 /**
- * Navbar component displays buttons to print nodes and edges.
+ * Navbar component displays buttons to manage all the logic of the website.
  * @returns {JSX.Element} Navbar component
  */
 const Navbar = () => {
   const { nodes, edges } = useContext(GraphParamsContext);
 
-  const runPrim = () => {
-    const graph = createGraphFromComponent(nodes, edges);
-    const result = prim(graph);
-    console.log(result);
+  const animatePrim = async () => {
+    const edgeIds = runPrim(nodes, edges);
+    startAnimations(edgeIds);
   };
 
-  const runDijkstra = () => {
-    const graph = createGraphFromComponent(nodes, edges);
-    const result = dijkstra(graph);
-    console.log(result);
+  const animateDijkstra = async () => {
+    const edgeIds = runDijkstra(nodes, edges);
+    startAnimations(edgeIds);
   };
 
   return (
     <>
-      <button onClick={runPrim}>Run prim's algorithm</button>
-      <button onClick={runDijkstra}>Run dijkstra's algorithm</button>
-      <button
-        onClick={() => {
-          console.log(nodes);
-        }}
-      >
-        Print nodes
-      </button>
-      <button
-        onClick={() => {
-          console.log(edges);
-        }}
-      >
-        Print edges
-      </button>
+      <button onClick={animatePrim}>Run prim's algorithm</button>
+      <button onClick={animateDijkstra}>Run dijkstra's algorithm</button>
     </>
   );
 };
