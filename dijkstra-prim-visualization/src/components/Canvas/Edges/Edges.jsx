c226ee6d4faa4e623bda8569ed8a7f32621a03c6
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2023-11-19 03:51:30 +0100

    Refactor Edges component to render edge weights

--- dijkstra-prim-visualization/src/components/Canvas/Edges/Edges.jsx
+++ dijkstra-prim-visualization/src/components/Canvas/Edges/Edges.jsx
@@ -3,17 +3,11 @@ import React from "react";
 import Edge from "./Edge/Edge";
 
 /**
- * Renders edges on the canvas.
- * @param {Object[]} edges - An array of edge objects.
- * @param {string} edges[].id - The unique identifier of the edge.
- * @param {Object} edges[].firstNode - The first node of the edge.
- * @param {number} edges[].firstNode.x - The x-coordinate of the first node.
- * @param {number} edges[].firstNode.y - The y-coordinate of the first node.
- * @param {Object} edges[].secondNode - The second node of the edge.
- * @param {number} edges[].secondNode.x - The x-coordinate of the second node.
- * @param {number} edges[].secondNode.y - The y-coordinate of the second node.
- * @param {number} edges[].weight - The weight of the edge.
- * @returns {JSX.Element} - The rendered edges and their weights.
+ * Renders the edges on the canvas.
+ *
+ * @component
+ * @param {Object[]} edges - The array of edges to be rendered.
+ * @returns {JSX.Element} The rendered edges.
  */
 const Edges = ({ edges }) => {
   return (
@@ -26,14 +20,8 @@ const Edges = ({ edges }) => {
             y1={edge.firstNode.y}
             x2={edge.secondNode.x}
             y2={edge.secondNode.y}
+            weight={edge.weight}
           />
-          <text
-            x={(edge.firstNode.x + edge.secondNode.x) / 2}
-            y={(edge.firstNode.y + edge.secondNode.y) / 2}
-            fill="white"
-          >
-            {edge.weight}
-          </text>
         </React.Fragment>
       ))}
     </>
