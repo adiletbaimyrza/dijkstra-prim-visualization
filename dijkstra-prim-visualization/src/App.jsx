329cd108a1827b692c127c60a44e30108e8f5cf7
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2023-11-22 15:23:22 +0100

    Update ErrorModalProvider import

--- dijkstra-prim-visualization/src/App.jsx
+++ dijkstra-prim-visualization/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Title from "./components/Title/Title";
 import Instructions from "./components/Instructions/Instructions";
 import { GraphParamsProvider } from "./contexts/GraphParamsContext";
-import { ErrorModalProvider } from "./contexts/ErrorModalContext";
+import { ErrorModalProvider } from "./contexts/ModalsContext";
 
 function App() {
   return (
