defb2471cc7c6fb792f0c268524d89d2d33019f8
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2023-11-30 15:32:12 +0100

    Update edge stroke width in Navbar animations

--- dijkstra-prim-visualization/src/components/Navbar/animations.js
+++ dijkstra-prim-visualization/src/components/Navbar/animations.js
@@ -1,12 +1,13 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const GREEN = "#3fb950";
-const PURPLE = "#c709cb";
-const YELLOW = "yellow";
+const UNSELECT_EDGE_COLOR = "#3fb950";
+const SELECTED_EDGE_COLOR = "#c709cb";
+const CHECKED_EDGE_COLOR = "yellow";
 const INITIAL_EDGE_COLOR = "#33b3ae";
 const INITIAL_STROKE_WIDTH = "2";
 const SELECT_STROKE_WIDTH = "8";
 const UNSELECT_STROKE_WIDTH = "3";
+const CANDIDATE_STROKE_WIDTH = "12";
 
 const setTotalWeight = (weight) => {
   const totalWeightContainer = document.getElementById("totalWeight");
@@ -22,7 +23,7 @@ const highlightResultPath = (animationsData) => {
   if (animationsData.algorithmType === "prim") {
     for (const step of animationsData.stepsWithIds) {
       const edge = document.getElementById(step.selectedEdgeId);
-      edge.style.stroke = PURPLE;
+      edge.style.stroke = SELECTED_EDGE_COLOR;
       edge.style.strokeWidth = SELECT_STROKE_WIDTH;
     }
   } else if (animationsData.algorithmType === "dijkstra") {
@@ -72,7 +73,8 @@ const startAnimations = async (animationsData, speed, setAnimating) => {
   for (const step of animationsData.stepsWithIds) {
     for (const edgeId of step.checkedEdgeIds) {
       const edge = document.getElementById(edgeId);
-      edge.style.stroke = YELLOW;
+      edge.style.stroke = CHECKED_EDGE_COLOR;
+      edge.style.strokeWidth = CANDIDATE_STROKE_WIDTH;
 
       await sleep(500 / speed);
 
@@ -83,7 +85,7 @@ const startAnimations = async (animationsData, speed, setAnimating) => {
 
     const edge = document.getElementById(step.selectedEdgeId);
 
-    edge.style.stroke = GREEN;
+    edge.style.stroke = UNSELECT_EDGE_COLOR;
     edge.style.strokeWidth = SELECT_STROKE_WIDTH;
 
     if (animationsData.algorithmType === "prim") {
