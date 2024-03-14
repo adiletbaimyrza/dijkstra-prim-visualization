d48a95e661dc46b040e602a191d30b3c03f56f9f
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-03-14 12:21:08 +0100

    Refactor Edges component to remove unnecessary React.Fragment

--- dijkstra-prim-visualization/src/components/Canvas/Edges/Edges.jsx
+++ dijkstra-prim-visualization/src/components/Canvas/Edges/Edges.jsx
@@ -1,21 +1,19 @@
 import PropTypes from "prop-types";
-import React from "react";
 import Edge from "./Edge";
 
 const Edges = ({ edges }) => {
   return (
     <>
       {edges.map((edge) => (
-        <React.Fragment key={edge.id}>
-          <Edge
-            id={edge.id}
-            x1={edge.firstNode.x}
-            y1={edge.firstNode.y}
-            x2={edge.secondNode.x}
-            y2={edge.secondNode.y}
-            weight={edge.weight}
-          />
-        </React.Fragment>
+        <Edge
+          key={edge.id}
+          id={edge.id}
+          x1={edge.firstNode.x}
+          y1={edge.firstNode.y}
+          x2={edge.secondNode.x}
+          y2={edge.secondNode.y}
+          weight={edge.weight}
+        />
       ))}
     </>
   );
