9e10c0e3e44bba35c0406e30a8fd4cfc1c9e4e3f
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-03-14 14:46:11 +0100

    add barren exports

---
+++ dijkstra-prim-visualization/src/components/index.js
@@ -0,0 +1,6 @@
+import Canvas from "./Canvas/Canvas";
+import Indicator from "./Indicator/Indicator";
+import Navbar from "./Navbar/Navbar";
+import Title from "./Title/Title";
+
+export { Canvas, Indicator, Navbar, Title };
--- dijkstra-prim-visualization/src/contexts/index.js
+++ dijkstra-prim-visualization/src/contexts/index.js
@@ -1,6 +1,7 @@
 import { GraphParamsContext, GraphParamsProvider } from "./GraphParamsContext";
 import { ModalContext, ModalProvider } from "./ModalsContext";
 import { SavedGraphsContext, SavedGraphsProvider } from "./SavedGraphsContext";
+import Providers from "./Providers";
 
 export {
   GraphParamsContext,
@@ -9,4 +10,5 @@ export {
   ModalProvider,
   SavedGraphsContext,
   SavedGraphsProvider,
+  Providers,
 };
