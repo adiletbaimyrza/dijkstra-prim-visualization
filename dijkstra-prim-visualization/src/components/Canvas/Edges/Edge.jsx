14abd5d98cc1f42b5a662a42ac3ba8237d8f7be6
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-03-14 12:20:50 +0100

    refactor classes to styles

--- dijkstra-prim-visualization/src/components/Canvas/Edges/Edge.jsx
+++ dijkstra-prim-visualization/src/components/Canvas/Edges/Edge.jsx
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
-import classes from "./Edge.module.css";
+import styles from "./Edge.module.css";
 
 const Edge = ({ id, x1, y1, x2, y2, weight }) => {
   return (
     <g>
-      <line className={classes.line} id={id} x1={x1} y1={y1} x2={x2} y2={y2} />
+      <line className={styles.line} id={id} x1={x1} y1={y1} x2={x2} y2={y2} />
       <text
         id={`${id}-weight`}
-        className={classes.text}
+        className={styles.text}
         x={(x1 + x2) / 2}
         y={(y1 + y2) / 2}
         fill="white"
